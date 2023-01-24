d167737cb0b5f116a786dc3c6e96911f365cd5b7
Adrian Vincent Aguilar <aguilaradrianvincent@gmail.com>
2023-01-24 13:52:53 +0800

    feat: set tab spacing to 4

--- macos/nvim/lua/ava/set.lua
+++ macos/nvim/lua/ava/set.lua
@@ -4,6 +4,11 @@ vim.opt.relativenumber = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
