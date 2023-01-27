f710e3814e1355b016fdf159263cdd04203d5d66
Adrian Vincent Aguilar <aguilaradrianvincent@gmail.com>
2023-01-27 10:56:12 +0800

    refactor: modular config

--- macos/nvim/lua/ava/init.lua
+++ macos/nvim/lua/ava/init.lua
@@ -1,2 +1,5 @@
-require("ava.set")
-require("ava.remap")
+if not require("ava.packer") then
+    require("ava.set")
+    require("ava.remap")
+    require("ava.plugin-config")
+end
---
+++ macos/nvim/lua/ava/plugin-config/init.lua
@@ -0,0 +1,8 @@
+local base = "ava.plugin-config."
+require(base.."colors")
+require(base.."fugitive")
+require(base.."harpoon")
+require(base.."lsp")
+require(base.."telescope")
+require(base.."treesitter")
+require(base.."undotree")
