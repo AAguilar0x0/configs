9ee1dbc334f4f6aa39093781ed233349ba273b23
Adrian Vincent Aguilar <aguilaradrianvincent@gmail.com>
2023-10-09 12:26:20 +0800

    feat: add gopls and pin indent-backline version

--- nvim/kickstart/init.lua
+++ nvim/kickstart/init.lua
@@ -50,7 +50,10 @@ require('packer').startup(function(use)
 
   use 'navarasu/onedark.nvim' -- Theme inspired by Atom
   use 'nvim-lualine/lualine.nvim' -- Fancier statusline
-  use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
+  use {
+    'lukas-reineke/indent-blankline.nvim',
+    tag = 'v2.20.8',
+  } -- Add indentation guides even on blank lines
   use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
   use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
 
--- nvim/nvim/lua/packer-config.lua
+++ nvim/nvim/lua/packer-config.lua
@@ -60,7 +60,10 @@ require('packer').startup(function(use)
     }
 
     use 'nvim-lualine/lualine.nvim' -- Fancier statusline
-    use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
+    use {
+      'lukas-reineke/indent-blankline.nvim',
+      tag = 'v2.20.8',
+    } -- Add indentation guides even on blank lines
     use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
     use {
         "windwp/nvim-autopairs",
