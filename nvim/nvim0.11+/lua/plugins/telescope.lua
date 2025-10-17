ec587d312e380f79023f08b4a78d2f48e463a8f3
Adrian Vincent Aguilar <aguilaradrianvincent@gmail.com>
2025-10-17 10:47:53 +0800

    feat(telescope.lua): use ripgrep specifically and show hidden
    - don’t search inside files for text — just list all files that exist.
    - exclude .git directory

--- nvim/nvim0.11+/lua/plugins/telescope.lua
+++ nvim/nvim0.11+/lua/plugins/telescope.lua
@@ -10,9 +10,22 @@ return {
   },
   config = function()
     local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+    vim.keymap.set('n', '<leader>ff', function()
+      builtin.find_files({
+        -- hidden = true, -- show dotfiles
+        -- no_ignore = false, -- still respect .gitignore
+        -- no_ignore_parent = false,
+        find_command = {
+          'rg', -- use ripgrep
+          '--files',
+          '--hidden',
+          '--glob',
+          '!.git/*', -- ignore .git directory
+        },
+      })
+    end, {})
     vim.keymap.set('n', '<leader>ps', function()
-      builtin.grep_string({ search = vim.fn.input("Grep > ") })
+      builtin.grep_string({ search = vim.fn.input('Grep > ') })
     end)
     vim.keymap.set('n', '<leader>bs', function()
       builtin.current_buffer_fuzzy_find()
